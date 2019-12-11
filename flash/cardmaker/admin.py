1edfca4608e0fc12dd066baebe0e0408c70280de
unknown <tdijkmans@gmail.com>
2019-12-11 11:58:47 +0100

    Third Commit

--- flash/cardmaker/admin.py
+++ flash/cardmaker/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Student, Stack, Card
+
+class StudentAdmin(admin.ModelAdmin):
+    fields = ['full_name']
+
+
+class StackAdmin(admin.ModelAdmin):
+    fields = ['subject', 'student']
+
+
+class CardAdmin(admin.ModelAdmin):
+    fields = ['question', 'answer', 'subject', 'student']
+
+
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Stack, StackAdmin)
+admin.site.register(Card, CardAdmin)
--- flash/cardmaker/models.py
+++ flash/cardmaker/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    full_name =  models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Stack(models.Model):
+    subject = models.CharField(max_length=200)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.subject
+
+
+class Card(models.Model):
+    question = models.CharField(max_length=200)
+    answer = models.CharField(max_length=200)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Stack, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.question
